4cbde04e7fba15ad2d9a4bca49975ad03809783c
YepJared <97353828+YepJared@users.noreply.github.com>
2025-03-20 20:14:54 -0400

    Convert home page to generate HTML using JS
    Convert HTML for home page to JS using dom manipulation so that pages can eventually be dynamically built
    Refactoring to happen in a later commit

--- src/index.js
+++ src/index.js
@@ -1,1 +1,67 @@
 console.log("It works now!");
+
+function buildUI() {
+    const contentHolder = document.querySelector("#content");
+
+    // Build company info
+    const infoContainer = document.createElement("div");
+    infoContainer.classList.add("info");
+
+    const infoHeader = document.createElement("h1");
+    infoHeader.textContent = "Placeholder";
+    infoHeader.classList.add("name");
+
+    const infoDescription = document.createElement("p");
+    infoDescription.textContent = "This is going to be a blurb about the restaurant.";
+
+    infoContainer.append(
+        infoHeader,
+        infoDescription
+    );
+
+    // Build restaurant hours
+    const hoursContainer = document.createElement("div");
+    hoursContainer.classList.add("hours");
+
+    const hourHeader = document.createElement("h2");
+    hourHeader.textContent = "Hours";
+
+    const hourList = document.createElement("ol");
+    hourList.classList.add("schedule");
+
+    for (let i = 1; i <= 7; i++) {
+        const day = document.createElement("li");
+        day.classList.add("day");
+        day.textContent = i;
+        hourList.appendChild(day);
+    }
+
+    hoursContainer.append(
+        hourHeader,
+        hourList
+    );
+
+    // Build restaurant location
+    const locationContainer = document.createElement("div");
+    locationContainer.classList.add("location");
+
+    const locationHeader = document.createElement("h2");
+    locationHeader.textContent = "Address";
+
+    const locationText = document.createElement("p");
+    locationText.textContent = "address";
+
+    locationContainer.append(
+        locationHeader,
+        locationText
+    );
+
+    // Add to container
+    contentHolder.append(
+        infoContainer,
+        hoursContainer,
+        locationContainer
+    );
+}
+
+buildUI();
